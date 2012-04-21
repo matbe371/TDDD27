56825e067648a1287d2e0adbbb1fd309f1babbc8
Mathias <matbe371@student.liu.se>
2012-04-21 13:58:32 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TDDD27::Application.config.secret_token = '58e127aee19bf10310e3260405f62adb9426313340ab81c3d45586394b03d1ff16a95ca4e2e821bcdfbb6d3e0541d79861c80f95066baa76a924959ed0f01efd'
