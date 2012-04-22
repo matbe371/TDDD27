447ea516ef240dbe2a4332f15fdf4652c4db7bdf
Mathias <matbe371@student.liu.se>
2012-04-22 14:06:53 +0200

    Laggt till i quizz controller sa att det gar att slumpa fram sex artister i view

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,6 +1,36 @@
 class QuizController < ApplicationController
   def play
     @title = "Play Quiz"
+    
+    number_of_artists = Artist.count #Antalet artister i databasen.
+    first_artist_id = Artist.first.id 
+
+    @artists = Array.new
+           
+    #Fyller arrayen med sex st slumpmässiga, unika tal.
+    for i in 0..5  
+      random = rand(number_of_artists-first_artist_id) + first_artist_id #Skapar ett slumpat tal baretar på antalen artister i databasen
+      
+      if @artists.include? random
+        
+        while @artists.include? random
+          random = rand(number_of_artists-first_artist_id) + first_artist_id
+        end
+          @artists.push(random)
+      else
+        @artists.push(random)
+      end
+    end
+    
+    #Skapar de sex artister som kommer synas på sidan.
+    @artist1 = Artist.find(@artists[0])
+    @artist2 = Artist.find(@artists[1])
+    @artist3 = Artist.find(@artists[2])
+    @artist4 = Artist.find(@artists[3])
+    @artist5 = Artist.find(@artists[4])
+    @artist6 = Artist.find(@artists[5])
+    
+    
   end
 
   def result
