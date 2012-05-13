f22c633dd920d88afbb5c9f19e91eaad4b4850b5
Mathias <matbe371@student.liu.se>
2012-05-13 15:49:46 +0200

    Laggt till design

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -4,10 +4,10 @@ class QuizController < ApplicationController
   #Action för att berkäfta quiz
   def confirm
     @title = "Confirm"
-    user = User.first #TODO ändra till sessionuser.
+    @user = User.first #TODO ändra till sessionuser.
     
     #Skapar en ny quiz.
-    @new_quiz = user.quizzes.create(:score=>0, :genre=>"All")
+    @new_quiz = @user.quizzes.create(:score=>0, :genre=>"All")
     
     #Skapar en playlist tillhörnade quizzen, som kommer att visas när quizzet är avklarat. Detta sker mha relationerna i databasen.
     @new_quiz.playlists.create
@@ -25,157 +25,163 @@ class QuizController < ApplicationController
     @quiz = Quiz.find(quizId)
     
     #En räknare som håller koll på vilken fråga i ordningen vi är på. Fås som parameter.
-    @question_no = params[:question_no] #TODO skall fås om parameter.
+    @question_no = Integer(params[:question_no])
     
-    #TODO här skall det in en if-sats som kollar vilken fråga vi är på om 10 => redirect.
-    
-    #Array som håller koll på vilka artister som är med i frågeomgången. Det skall inte få förekomma två frågor med samma artist.
-    featured_artists = Array.new
-    
-    #Arrayer för frågor som är med i frågeomgången. Två av varje nivå. Instansvariabler som kommer finnas tillgängliga i vyn.
-    @easy_questions = Array.new
-    @medium_questions = Array.new
-    @hard_questions = Array.new
-    
-    #Arrayer som håller koll på informationen om frågorna på olika nivåer.
-    @easy_info = Array.new
-    @medium_info = Array.new
-    @hard_info = Array.new
-    
-    #Arrayer som innehåller information om ULR till YouTube, som används för att generera thumbnails.
-    @url_array_easy = Array.new
-    @url_array_medium = Array.new
-    @url_array_hard = Array.new
-    
-    
-    #***STEG 1 - Slumpar två triviafrågor***
-   
-    number_of_trivia_questions = TriviaQuestion.count #Antalet triviafrågor i databasen.
-    first_trivia_question_id = TriviaQuestion.first.id #ID på fråga nummer ett. 
-    
-    #Loopar två varv för att ta fram två triviafrågor.
-    for i in 0..1
+    #Quizzet består av 10 frågor. Dvs har man kommit till fråga 11 är quizzet över.
+    if @question_no == 11
+      @tmp = "test"
+      #Länkas automatiskt vidare till sidan som visar resultatet. 
+      redirect_to result_path(params={:quiz_id=>@quiz.id})
+    #Annars ska användaren få välja en till fråga. 
+    else 
+      #Array som håller koll på vilka artister som är med i frågeomgången. Det skall inte få förekomma två frågor med samma artist.
+      featured_artists = Array.new
       
-      #Slumpar ett random-nummer baserat på antalet frågor i databasen och hämtar frågan med detta id.
-      random = rand((number_of_trivia_questions+1)-first_trivia_question_id) + first_trivia_question_id
-      question = TriviaQuestion.find(random)
+      #Arrayer för frågor som är med i frågeomgången. Två av varje nivå. Instansvariabler som kommer finnas tillgängliga i vyn.
+      @easy_questions = Array.new
+      @medium_questions = Array.new
+      @hard_questions = Array.new
+      
+      #Arrayer som håller koll på informationen om frågorna på olika nivåer.
+      @easy_info = Array.new
+      @medium_info = Array.new
+      @hard_info = Array.new
+      
+      #Arrayer som innehåller information om ULR till YouTube, som används för att generera thumbnails.
+      @url_array_easy = Array.new
+      @url_array_medium = Array.new
+      @url_array_hard = Array.new
+      
+      
+      #***STEG 1 - Slumpar två triviafrågor***
      
-      #Om artisten har förekommit i en tidigare fråga eller frågan redan har förekommit i samma omgång slumpas en ny fram.
-      while featured_artists.include? question.artist.id 
+      number_of_trivia_questions = TriviaQuestion.count #Antalet triviafrågor i databasen.
+      first_trivia_question_id = TriviaQuestion.first.id #ID på fråga nummer ett. 
+      
+      #Loopar två varv för att ta fram två triviafrågor.
+      for i in 0..1
+        
+        #Slumpar ett random-nummer baserat på antalet frågor i databasen och hämtar frågan med detta id.
         random = rand((number_of_trivia_questions+1)-first_trivia_question_id) + first_trivia_question_id
         question = TriviaQuestion.find(random)
+       
+        #Om artisten har förekommit i en tidigare fråga eller frågan redan har förekommit i samma omgång slumpas en ny fram.
+        while featured_artists.include? question.artist.id 
+          random = rand((number_of_trivia_questions+1)-first_trivia_question_id) + first_trivia_question_id
+          question = TriviaQuestion.find(random)
+        end
+        
+        #Stoppar in värden i minnesarrayerna.
+        featured_artists.push(question.artist.id) 
+        
+        #Skapar en URL-källa för thumbnail, som hämtas från YouTube, baserat på ID't för klippet i databasen.
+        thumbnail_src = "http://img.youtube.com/vi/#{question.artist.songs.find(question.artist.id).youtube_url}/2.jpg"
+        
+        #Stoppar in frågan med tillhörande information i rätt arrayer, baserat på svårighetsgraden.
+        if question.level == "easy"
+          @easy_questions.push(question)
+          @easy_info.push(question.artist.name)
+          @easy_info.push("Trivia")
+          @url_array_easy.push(thumbnail_src)
+        elsif question.level == "medium"
+          @medium_questions.push(question)
+          @medium_info.push(question.artist.name)
+          @medium_info.push("Trivia")
+          @url_array_medium.push(thumbnail_src)
+        elsif question.level == "hard"
+          @hard_questions.push(question)
+          @hard_info.push(question.artist.name)
+          @hard_info.push("Trivia")
+          @url_array_hard.push(thumbnail_src)
+        end
+        
       end
       
-      #Stoppar in värden i minnesarrayerna.
-      featured_artists.push(question.artist.id) 
+      #***STEG 2 - Slumpar två sångfrågor***
+      #Algoritmen är liknande, men nu tillkommer även en koll om arrayen för frågenivån är full.
       
-      #Skapar en URL-källa för thumbnail, som hämtas från YouTube, baserat på ID't för klippet i databasen.
-      thumbnail_src = "http://img.youtube.com/vi/#{question.artist.songs.find(question.artist.id).youtube_url}/2.jpg"
+      number_of_song_questions = SongQuestion.count #Antalet sångfrågor i databasen.
+      first_song_question_id = SongQuestion.first.id #ID på fråga nummer ett. 
       
-      #Stoppar in frågan med tillhörande information i rätt arrayer, baserat på svårighetsgraden.
-      if question.level == "easy"
-        @easy_questions.push(question)
-        @easy_info.push(question.artist.name)
-        @easy_info.push("Trivia")
-        @url_array_easy.push(thumbnail_src)
-      elsif question.level == "medium"
-        @medium_questions.push(question)
-        @medium_info.push(question.artist.name)
-        @medium_info.push("Trivia")
-        @url_array_medium.push(thumbnail_src)
-      elsif question.level == "hard"
-        @hard_questions.push(question)
-        @hard_info.push(question.artist.name)
-        @hard_info.push("Trivia")
-        @url_array_hard.push(thumbnail_src)
-      end
-      
-    end
-    
-    #***STEG 2 - Slumpar två sångfrågor***
-    #Algoritmen är liknande, men nu tillkommer även en koll om arrayen för frågenivån är full.
-    
-    number_of_song_questions = SongQuestion.count #Antalet sångfrågor i databasen.
-    first_song_question_id = SongQuestion.first.id #ID på fråga nummer ett. 
-    
-    for i in 0..1
-      
-      random = rand((number_of_song_questions+1)-first_song_question_id) + first_song_question_id
-      question = SongQuestion.find(random)
-      
-      
-      #Här tillkommer ett villkor som kollar om frågearrayerna redan är fullsatta. I så fall slumpas en ny fråga.
-      while (featured_artists.include? question.song.artist.id) || (question.level == "easy" && @easy_questions.size == 2) || (question.level == "medium" && @medium_questions.size == 2) || (question.level == "hard" && @hard_questions.size == 2) 
+      for i in 0..1
+        
         random = rand((number_of_song_questions+1)-first_song_question_id) + first_song_question_id
         question = SongQuestion.find(random)
+        
+        
+        #Här tillkommer ett villkor som kollar om frågearrayerna redan är fullsatta. I så fall slumpas en ny fråga.
+        while (featured_artists.include? question.song.artist.id) || (question.level == "easy" && @easy_questions.size == 2) || (question.level == "medium" && @medium_questions.size == 2) || (question.level == "hard" && @hard_questions.size == 2) 
+          random = rand((number_of_song_questions+1)-first_song_question_id) + first_song_question_id
+          question = SongQuestion.find(random)
+        end
+        
+        featured_artists.push(question.song.artist.id) 
+        
+        thumbnail_src = "http://img.youtube.com/vi/#{question.song.youtube_url}/2.jpg"
+  
+        if question.level == "easy"
+          @easy_questions.push(question)
+          @easy_info.push(question.song.artist.name)
+          @easy_info.push("Song")
+          @url_array_easy.push(thumbnail_src)
+        elsif question.level == "medium"
+          @medium_questions.push(question)
+          @medium_info.push(question.song.artist.name)
+          @medium_info.push("Song")
+          @url_array_medium.push(thumbnail_src)
+        elsif question.level == "hard"
+          @hard_questions.push(question)
+          @hard_info.push(question.song.artist.name)
+          @hard_info.push("Song")
+          @url_array_hard.push(thumbnail_src)
+        end
       end
       
-      featured_artists.push(question.song.artist.id) 
       
-      thumbnail_src = "http://img.youtube.com/vi/#{question.song.youtube_url}/2.jpg"
-
-      if question.level == "easy"
-        @easy_questions.push(question)
-        @easy_info.push(question.song.artist.name)
-        @easy_info.push("Song")
-        @url_array_easy.push(thumbnail_src)
-      elsif question.level == "medium"
-        @medium_questions.push(question)
-        @medium_info.push(question.song.artist.name)
-        @medium_info.push("Song")
-        @url_array_medium.push(thumbnail_src)
-      elsif question.level == "hard"
-        @hard_questions.push(question)
-        @hard_info.push(question.song.artist.name)
-        @hard_info.push("Song")
-        @url_array_hard.push(thumbnail_src)
-      end
-    end
-    
-    
-    #***STEG 3 - Slumpar två albumfrågor***
-    #Algoritmen är liknande den för sångfrågor.
-    
-    number_of_album_questions = AlbumQuestion.count #Antalet albumfrågor i databasen.
-    first_album_question_id = AlbumQuestion.first.id #ID på fråga nummer ett. 
-    
-    for i in 0..1
+      #***STEG 3 - Slumpar två albumfrågor***
+      #Algoritmen är liknande den för sångfrågor.
       
-      random = rand((number_of_album_questions+1)-first_album_question_id) + first_album_question_id
-      question = AlbumQuestion.find(random)
+      number_of_album_questions = AlbumQuestion.count #Antalet albumfrågor i databasen.
+      first_album_question_id = AlbumQuestion.first.id #ID på fråga nummer ett. 
       
-      while (featured_artists.include? question.album.artist.id) || (question.level == "easy" && @easy_questions.size == 2) || (question.level == "medium" && @medium_questions.size == 2) || (question.level == "hard" && @hard_questions.size == 2) 
+      for i in 0..1
+        
         random = rand((number_of_album_questions+1)-first_album_question_id) + first_album_question_id
-        question = AlbumQuestion.find(random)  
-      end
-      
-      #Stoppar in värden i minnesarrayerna.
-      featured_artists.push(question.album.artist.id) 
-      
-      thumbnail_src = "http://img.youtube.com/vi/#{question.album.youtube_url}/2.jpg"
-      
-      #Stoppar in frågan i rätt array, baserat på svårighetsgraden.
-      if question.level == "easy"
-        @easy_questions.push(question)
-        @easy_info.push(question.album.artist.name)
-        @easy_info.push("Album")
-        @url_array_easy.push(thumbnail_src)
-      elsif question.level == "medium"
-        @medium_questions.push(question)
-        @medium_info.push(question.album.artist.name)
-        @medium_info.push("Album")
-        @url_array_medium.push(thumbnail_src)
-      elsif question.level == "hard"
-        @hard_questions.push(question)
-        @hard_info.push(question.album.artist.name)
-        @hard_info.push("Album")
-        @url_array_hard.push(thumbnail_src)
-      end
-    end 
-    
+        question = AlbumQuestion.find(random)
+        
+        while (featured_artists.include? question.album.artist.id) || (question.level == "easy" && @easy_questions.size == 2) || (question.level == "medium" && @medium_questions.size == 2) || (question.level == "hard" && @hard_questions.size == 2) 
+          random = rand((number_of_album_questions+1)-first_album_question_id) + first_album_question_id
+          question = AlbumQuestion.find(random)  
+        end
+        
+        #Stoppar in värden i minnesarrayerna.
+        featured_artists.push(question.album.artist.id) 
+        
+        thumbnail_src = "http://img.youtube.com/vi/#{question.album.youtube_url}/2.jpg"
+        
+        #Stoppar in frågan i rätt array, baserat på svårighetsgraden.
+        if question.level == "easy"
+          @easy_questions.push(question)
+          @easy_info.push(question.album.artist.name)
+          @easy_info.push("Album")
+          @url_array_easy.push(thumbnail_src)
+        elsif question.level == "medium"
+          @medium_questions.push(question)
+          @medium_info.push(question.album.artist.name)
+          @medium_info.push("Album")
+          @url_array_medium.push(thumbnail_src)
+        elsif question.level == "hard"
+          @hard_questions.push(question)
+          @hard_info.push(question.album.artist.name)
+          @hard_info.push("Album")
+          @url_array_hard.push(thumbnail_src)
+        end
+      end 
+    #end - översta if-satasen
+    end
     #**Klart! nu finns all infomration som behövs i play.html.erb**
-  end
-
+  end 
+  
   #Action för att visa den fråga som man har klickat på.
   def show_question
     
@@ -261,11 +267,15 @@ class QuizController < ApplicationController
     
     #Kollar användaren har svarat rätt på frågan.
     #Squish gör så att blanktecken i början och slutet ignoreras. Dessutom blir multipla blanktecken till ett.
+    squishAnswer = answer.squish
+    squishUserAnswer = user_answer.squish
     #Titelize gör så att alla ord jämförs med stor bokstav först. Dvs. Det spelar inten roll om svar/fråga är skrivan med stor bokstav eller ej.
-    if answer.squish.titleize  == user_answer.squish.titleize  
+    if squishAnswer.titleize  == squishUserAnswer.titleize  
       @result = "Correct"
       #Uppdaterar quizzets score. Pointsvariabeln type-castas till en integer för att beräkning skall kunna utföras.
       @quiz.score = @quiz.score + Integer(points) 
+      #Uppdateringen sparas i databasen.
+      @quiz.save 
     else 
       @result = "Fail"
     end
@@ -277,6 +287,31 @@ class QuizController < ApplicationController
 
   #Action för resultatsidan.
   def result
+    #sidans titel
     @title = "Quiz results"
+    
+    #Det aktuella quizzet som spelas fås som parameter.
+    quizId = params[:quiz_id]
+    @quiz = Quiz.find(quizId)
+    
+    #Skapar arrayer för rätta svar och spellista.
+    @answers = Array.new
+    @playlist = Array.new
+    
+    #Lagrar över svaren hos quizzet i arrayen.
+    for i in 1..10
+      #Hämtar motsvarande svar med hjälp av where-villkoret.
+      tmp_answer = @quiz.answers.select(:answer).where(:question_number=>i).first.answer
+      #Stoppar in all information som kommer att redovisas.
+      @answers.push(tmp_answer)
+    end
+    
+    #Lagrar över spellistan hos quizzet i arrayen.
+    for j in 1..10
+      #Hämtar motsvarande playlist-item med hjälp av where-villkoret.
+      tmp_item = @quiz.playlists.first.playlist_items.where(:position=>j).first
+      @playlist.push(tmp_item.item_artist)
+      @playlist.push(tmp_item.item_song)
+    end
   end
 end
--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -1,2 +1,12 @@
 module QuizHelper
+  
+    #Metod för att returnera ett albums namn uten "(album)" på slutet.
+    def get_album_name(album)
+      #Albumets längt i antal tecken. 
+      total_length = album.length
+      #Längd minus de åtta sista tecknen = " (album)". 
+      title_lenght = total_length - 8
+      #Returnenrar (ruby har implicit returnering) nu bara albumtiteln.
+      album.first(title_lenght)
+    end
 end
