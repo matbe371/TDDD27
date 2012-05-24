78e68d79c26862e6c8df020e25f17b3bea061b6b
Mathias <matbe371@student.liu.se>
2012-05-24 14:04:25 +0200

    Fungerande nagigationsmeny

--- app/assets/javascripts/custom_scripts.js
+++ app/assets/javascripts/custom_scripts.js
@@ -20,38 +20,5 @@ $(document).ready(function(){
 				$("#dropdown-toggle").addClass("active");
 			}
 		});
-	
-	function setActive(site){
-		alert("test");
-		switch(site){
-			case "nav_home":
-				$("#nav_home").addClass("active");
-				$("#nav_help").removeClass("active");
-				$("#nav_new").removeClass("active");
-				$("#nav_login").removeClass("active");
-				//$("container").fadeIn(600);
-			break;
-			case "nav_help":
-				$("#nav_help").addClass("active");
-				$("#nav_home").removeClass("active");
-				$("#nav_new").removeClass("active");
-				$("#nav_login").removeClass("active");
-				//$("container").fadeIn(600);
-			break;
-			case "nav_new":
-				$("#nav_new").addClass("active");
-				$("#nav_home").removeClass("active");
-				$("#nav_help").removeClass("active");
-				$("#nav_login").removeClass("active");
-				//$("container").fadeIn(600);
-			break;
-				case "nav_login":
-				$("#nav_login").addClass("active");
-				$("#nav_home").removeClass("active");
-				$("#nav_help").removeClass("active");
-				$("#nav_new").removeClass("active");
-				//$("container").fadeIn(600);
-			break;
-	}
-	}
+		
 });
